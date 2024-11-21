5f6a1e71d0a11cacea0189848ed91861314cb7f6
Andy Kuo <zhenting@guozhentingdeMacBook-Air.local>
2024-11-21 12:23:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import torch
 from imagenet_stubs.imagenet_2012_labels import label_to_name
 
 
-img = Image.open(urlopen(
-'https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png'))
+img = Image.open(urlopen('https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png'))
 # Load the MobileNet model from timm
 model = timm.create_model('mobilenetv4_conv_small.e2400_r224_in1k', pretrained=True)
 model.eval()
+
+# Check if MPS is available and move model to MPS if it is
+device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+model.to(device)
+print(f"Model Loaded Successfully on {device}")
 
 # get model specific transforms (normalization, resize)
 data_config = timm.data.resolve_model_data_config(model)
@@ -20,16 +24,12 @@ with torch.no_grad():
     output = model(transforms(img).unsqueeze(0))  # unsqueeze single image into batch of 1
 top5_probabilities, top5_class_indices = torch.topk(output.softmax(dim=1), k=5) 
 # batch_size = 1
-top5_class_indices = top5_class_indices.cpu().numpy().tolist()[0]
-top5_probabilities = top5_probabilities.cpu().numpy().tolist()[0]
+top5_class_indices = top5_class_indices.numpy().tolist()[0]
+top5_probabilities = top5_probabilities.numpy().tolist()[0]
 
 for idx, prob in zip(top5_class_indices, top5_probabilities):
     print(label_to_name(idx), prob)
 
-# Check if MPS is available and move model to MPS if it is
-device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
-model.to(device)
-print(f"Model Loaded Successfully on {device}")
 
 
 
